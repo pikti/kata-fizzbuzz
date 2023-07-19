301b83de200319c2ead2c33be61408d64409b48a
pikti <32124359+pikti@users.noreply.github.com>
2023-07-19 14:37:20 +0200

    Add v2

--- FizzBuzzService.Tests/FizzBuzzService.Tests.cs
+++ FizzBuzzService.Tests/FizzBuzzService.Tests.cs
@@ -60,5 +60,59 @@ namespace FizzBuzz.UnitTests.Services
             var result = _fizzBuzzService.GetAnswer(value);
             Assert.True(result == "FizzBuzz", $"Input: {value} | Result: {result}");
         }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-3)]
+        public void GetAnswerV2_InputIsLessThan1_ReturnArgumentOutOfRangeException(int value)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _fizzBuzzService.GetAnswerV2(value));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(44)]
+        public void GetAnswerV2_InputIsNotAMultipleOf3Or5AndNotContains3Or5_ReturnInput(int value)
+        {
+            var result = _fizzBuzzService.GetAnswerV2(value);
+            Assert.True(int.TryParse(result, out int nb) && nb == value, $"Input: {value} | Result: {result}");
+        }
+
+        [Theory]
+        [InlineData(9)]
+        [InlineData(43)]
+        public void GetAnswerV2_InputIsAMultipleOf3OrContains3_ReturnFizz(int value)
+        {
+            var result = _fizzBuzzService.GetAnswerV2(value);
+            Assert.True(result == "Fizz", $"Input: {value} | Result: {result}");
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(52)]
+        public void GetAnswerV2_InputIsAMultipleOf5OrContains5_ReturnBuzz(int value)
+        {
+            var result = _fizzBuzzService.GetAnswerV2(value);
+            Assert.True(result == "Buzz", $"Input: {value} | Result: {result}");
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(36)]
+        public void GetAnswerV2_InputIsAMultipleOf3AndContains3_ReturnFizzFizz(int value)
+        {
+            var result = _fizzBuzzService.GetAnswerV2(value);
+            Assert.True(result == "FizzFizz", $"Input: {value} | Result: {result}");
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(50)]
+        public void GetAnswerV2_InputIsAMultipleOf5AndContains5_ReturnBuzzBuzz(int value)
+        {
+            var result = _fizzBuzzService.GetAnswerV2(value);
+            Assert.True(result == "BuzzBuzz", $"Input: {value} | Result: {result}");
+        }
     }
 }
